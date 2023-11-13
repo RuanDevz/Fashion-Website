c41fff6ef506b5cc3692ce0fc39e586f99775f8e
RuanDevz <Ruanbatista1509@outlook.com>
2023-11-12 23:19:15 -0300

    header concluido

--- fashionwebsite/src/Componentes/Header.js
+++ fashionwebsite/src/Componentes/Header.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import logo from '../img/fashionlogo.png'
+import estilo from '../estilos/Header.css'
 
 const Header = () => {
   return (
     <div>
-      <h1>Fashion</h1>
+      <div className='Fashion-container'>
+          <img src={logo}></img>
+          <h1 className='Fashion-title'>Fashion</h1>
+        <ul className='Fashion-options'>
+        <li>Catalogo</li>
+        <li>Fashion</li>
+        <li>Favoritos</li>
+        <li>Lifestyle</li>
+        <li className='login'>Sing Up</li>
+      </ul>
+      </div>
     </div>
   )
 }
